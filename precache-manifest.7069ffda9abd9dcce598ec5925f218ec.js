7c6dc18fdd586ff18ac3a8543fc208b1ba99fe8d
Michał <michal.pieniazek@gmail.com>
2022-02-23 14:48:16 +0100

    Updates

---
+++ precache-manifest.7069ffda9abd9dcce598ec5925f218ec.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "01077775bd3a752b0d433b3152a4d62a",
+    "url": "/ReactRouterApp/index.html"
+  },
+  {
+    "revision": "b2a8319aea4edc3111c9",
+    "url": "/ReactRouterApp/static/css/main.e9a754b4.chunk.css"
+  },
+  {
+    "revision": "010cc1d0285ab5801827",
+    "url": "/ReactRouterApp/static/js/2.09b57529.chunk.js"
+  },
+  {
+    "revision": "bb895545e484ff0164d4",
+    "url": "/ReactRouterApp/static/js/3.71a42320.chunk.js"
+  },
+  {
+    "revision": "b2a8319aea4edc3111c9",
+    "url": "/ReactRouterApp/static/js/main.c3d44a51.chunk.js"
+  },
+  {
+    "revision": "7ce1f563b59706d78cba",
+    "url": "/ReactRouterApp/static/js/runtime~main.59942d88.js"
+  },
+  {
+    "revision": "70e6670ee746fc59f414fde4fcce93af",
+    "url": "/ReactRouterApp/static/media/header1.70e6670e.jpg"
+  },
+  {
+    "revision": "33eca35fceea8311d67358a6629fa9d9",
+    "url": "/ReactRouterApp/static/media/header2.33eca35f.jpg"
+  },
+  {
+    "revision": "d9d36b65a132d26aed884ad58e4719eb",
+    "url": "/ReactRouterApp/static/media/header3.d9d36b65.jpg"
+  }
+]);
